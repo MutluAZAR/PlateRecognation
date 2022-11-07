9484c4eea177de1cd2db96bb081ecbed160990c3
MutluAZAR <mutluazar718@gmail.com>
2022-11-07 08:06:28 +0300

    commit

--- PlateRecognation/OcrBase/Src/Constants.py
+++ PlateRecognation/OcrBase/Src/Constants.py
@@ -1,1 +1,1 @@
-OCR_CLASSIFIER_PATH = r"../models/291022_092354943555_all_char_svc_model_0.9665871121718377.pth"
+OCR_CLASSIFIER_PATH = r"D:\Development\new\PythonApplication\PlateRecognation\PlateRecognation\OcrBase\models\291022_092354943555_all_char_svc_model_0.9665871121718377.pth"
